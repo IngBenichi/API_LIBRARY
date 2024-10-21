12c85fda07e7edc5b3b6e96ee7a7f358f7fde917
benichi <cbenitez8@cuc.edu.co>
2024-10-21 00:48:53 -0500

    cuarto commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import numpy as np
-from pyDecision.algorithm import ahp_method
+from pyDecision.algorithm import ahp_method,fuzzy_ahp_method
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -58,3 +58,36 @@ def calculate_ahp(request: AHPRequest):
         "consistency_message": consistency_message,
         "dataset": dataset_list
     }
+
+
+
+
+
+# Dataset fijo
+dataset = [
+    [(1, 1, 1), (4, 5, 6), (3, 4, 5), (6, 7, 8)],   # g1
+    [(1/6, 1/5, 1/4), (1, 1, 1), (1/3, 1/2, 1), (2, 3, 4)],   # g2
+    [(1/5, 1/4, 1/3), (1, 2, 3), (1, 1, 1), (2, 3, 4)],   # g3
+    [(1/8, 1/7, 1/6), (1/4, 1/3, 1/2), (1/4, 1/3, 1/2), (1, 1, 1)]    # g4
+]
+
+@app.get("/fuzzy-ahp")
+
+def calculate_fuzzy_ahp():
+    try:
+        
+        fuzzy_weights, crisp_weigths, normalized_weights, rc = fuzzy_ahp_method(dataset)
+
+        
+        result = {
+            "dataset": dataset,
+            "fuzzy_weights": [list(np.around(weight, 3)) for weight in fuzzy_weights],
+            "crisp_weigths": [round(weight, 3) for weight in crisp_weigths],
+            "normalized_weights": [round(weight, 3) for weight in normalized_weights],
+            "consistency_ratio": round(rc, 2),
+            "consistency_message": "The solution is consistent" if rc <= 0.10 else "The solution is inconsistent, the pairwise comparisons must be reviewed"
+        }
+
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"An error occurred: {str(e)}")
